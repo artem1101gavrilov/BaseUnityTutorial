64a4ef626b073a22cea0c41a85226c9e9fff5c1d
Гаврилов Артем <dede13@yandex.ru>
2021-05-23 13:27:35 +0300

    Поворот объекта в 2D.

---
+++ BaseUnityTutorial/Assets/Atan/Scripts/TestAtan2Rotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAtan2Rotation : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> Sprites;
+    private SpriteRenderer spriteRenderer;
+    private List<TupleAngle> Angles = new List<TupleAngle> { new TupleAngle(-22.5f, 22.5f), new TupleAngle(22.5f, 67.5f), new TupleAngle(67.5f, 112.5f),
+        new TupleAngle(112.5f, 167.5f), new TupleAngle(-67.5f, -22.5f), new TupleAngle(-112.5f, -67.5f), new TupleAngle(-167.5f, -112.5f),
+        new TupleAngle(float.MinValue, float.MaxValue) };
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var direction = mousePosition - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        spriteRenderer.sprite = Sprites[Angles.FindIndex(x => angle >= x.MinAngle && angle <= x.MaxAngle)];
+    }
+}
+
+public struct TupleAngle
+{
+    public TupleAngle(float minAngle, float maxAngle)
+    {
+        MinAngle = minAngle;
+        MaxAngle = maxAngle;
+    }
+
+    public float MinAngle { get; set; }
+    public float MaxAngle { get; set; }
+}
---
+++ BaseUnityTutorial/Assets/Atan/Scripts/TestTanRotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestTanRotation : MonoBehaviour
+{
+    [SerializeField] private Transform Zombie;
+
+    private void Update()
+    {
+        var direction = Zombie.position - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
